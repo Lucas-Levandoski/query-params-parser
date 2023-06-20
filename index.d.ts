4b899df705391f9666a4b48bb1026f02e90bba1c
Lucas Levandoski <lucas-levandoski@hotmail.com>
2023-06-20 17:35:56 -0300

    fixing types

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,8 @@
-declare module 'query-params-parser';
+import { URLSearchParams } from "url";
 
+declare function parseParams <obj>(baseObject: obj, params?: URLSearchParams, arrSeparator?: string): Partial<obj>
+// export as default
+export = parseParams;
+
+export declare function parseByType <obj>(variable: string, property: obj, arrSeparator: string): obj;
+export declare function stringToBoolean (stringValue: string): bool;
